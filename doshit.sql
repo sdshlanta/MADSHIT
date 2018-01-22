73dbdd29dac5c96c8aea2f5d52b870d39e637d9d
Mike Shlanta <sdshlanta@gmail.com>
2018-01-22 14:56:56 -0600

    Fixed syntax errors

--- doshit.sql
+++ doshit.sql
@@ -1,9 +1,10 @@
-CREATE DATABASE doshit DEFAULT CHARACTER SET 'utf8'
+CREATE DATABASE doshit DEFAULT CHARACTER SET 'utf8';
 
-CREATE TABLE `ashit` (
-	`shit_no` int(11) NOT NULL AUTO_INCREMENT,
-	`shit_type` int(1) NOT NULL,
-	`shit_time` date NOT NULL,
-	`shit_length` int(64) NOT NULL,
-	PRIMARY KEY (`shit_no`)
-) ENGINE=InnoDBs
+USE doshit;
+
+CREATE TABLE ashit (
+	shit_no int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	shit_type int(1) NOT NULL,
+	shit_time date NOT NULL,
+	shit_length int(64) NOT NULL
+);
