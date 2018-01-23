2c8f3d68fd3aa0a51524f4dd5c9860a3219e5a56
Mike Shlanta <sdshlanta@gmail.com>
2018-01-23 13:56:18 -0600

    Added enableAlarm() Added disableAlarm() Set up intrrupts for the 4 different alerts Created way to cancel an alert

--- HARDSHIT.py
+++ HARDSHIT.py
@@ -3,43 +3,87 @@ import RPi.GPIO as GPIO
 import argparse
 import mysql.connector
 import time
+import threading
 from datetime import datetime
 
+aCurrentSHIT = None
+GPIO.setmode(GPIO.BOARD)
 
-GPIO.setmode(GPIO.BCM)
-
-channelMap = {
-	1:1,
-	2:2,
-	3:3,
-	4:4
+aSHITType = {
+	5:1,
+	6:2,
+	13:3,
+	19:4
 }
+
+def enableAlarm():
+	print("Enabled alarm")
+	GPIO.output(2, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH)
+
+def disableAlarm():
+	print("Disabled alarm")
+	GPIO.output(2, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH)
+	aCurrentSHIT = None
 
 
 def main():
+	latestSHITNo = args.lastAlarmNumber
+
 	def shitInterrupt(channel):
 		insertionQuery = "INSERT INTO ashit (shit_type, shit_length) VALUES (%d, %d)"
-		data = (channelMap[channel], args.testAlertLength)
-		dbConn = mysql.connector.connect(user=args.databaseUsername, password=args.databasePassword,
-									host=args.databaseHost,
-									database=args.databaseName)
+		data = (aSHITType[channel], args.testAlertLength)
+		
 		cur = dbConn.cursor()
-		print(insertionQuery % data)
+		# print(insertionQuery % data)
 		cur.execute(insertionQuery % data)
 		dbConn.commit()
-		print(cur.lastrowid)
-		cur.execute('SELECT * FROM ashit')
-		for row in cur:
-			print(row)
+		# print(cur.lastrowid)
+		# cur.execute('SELECT * FROM ashit')
+		# for row in cur:
+		# 	print(row)
 		
 		cur.close()
 		dbConn.close()
-	GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(2, GPIO.RISING, callback=shitInterrupt)
+	
+	GPIO.setup([5, 6, 13, 16], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup([3, 4], GPIO.OUT)
+
+	GPIO.add_event_detect(5, GPIO.RISING, callback=shitInterrupt)
+	GPIO.add_event_detect(6, GPIO.RISING, callback=shitInterrupt)
+	GPIO.add_event_detect(13, GPIO.RISING, callback=shitInterrupt)
+	GPIO.add_event_detect(16, GPIO.RISING, callback=shitInterrupt)
+	
 	try:
 		while True:
+			dbConn = mysql.connector.connect(user=args.databaseUsername, password=args.databasePassword,
+											host=args.databaseHost,
+											database=args.databaseName)
+			cur = dbConn.cursor()
+			cur.execute("SELECT shit_no, shit_length, shit_type FROM ashit ORDER BY shit_time DESC LIMIT 1")
+			for shit_no, shit_length, shit_type in cur:
+				if shit_no > latestSHITNo:
+					latestSHITNo = shit_no
+					if aCurrentSHIT is None:
+						enableAlarm()
+						aCurrentSHIT = threading.Timer(shit_length, disableAlarm)
+					elif shit_type == 5
+						if aCurrentSHIT:
+							aCurrentSHIT.cancel()
+							disableAlarm()
+			cur.close()
+			dbConn.close()
 			time.sleep(1)
 	except KeyboardInterrupt:
+		try:
+			cur.close()
+		except:
+			pass
+		try:
+			dbConn.close()
+		except:
+			pass
 		GPIO.cleanup()
 
 if __name__ == '__main__':
@@ -49,5 +93,6 @@ if __name__ == '__main__':
 	parser.add_argument("-U", "--databaseUsername", type=str, help="Username to be used for the database connection. Default is root", default="daSHIT")
 	parser.add_argument("-P", "--databasePassword", type=str, help="Password to be used for the database connection. Default is blank", default="")
 	parser.add_argument("-l", "--testAlertLength", type=int, help="The amount of time a test alert should last", default=5)
+	parser.add_argument("-a", "--lastAlarmNumber", type=int, help="The set the inital value for the last alarm", default = -1)
 	args = parser.parse_args()
 	main()
