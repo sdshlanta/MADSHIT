7ed4fb1ef910d2f19979e8b2a06c2da3f0c5a48b
Mike Shlanta <sdshlanta@gmail.com>
2018-01-23 13:57:02 -0600

    fixed shit_type

--- doshit.sql
+++ doshit.sql
@@ -4,7 +4,7 @@ USE doshit;
 
 CREATE TABLE ashit (
 	shit_no int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	shit_type int(1) NOT NULL,
+	shit_type int(3) NOT NULL,
 	shit_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	shit_length int(64) NOT NULL
 );
