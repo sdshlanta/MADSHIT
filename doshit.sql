009758a934baf0845756cd0320c13e11fb4fa148
Mike Shlanta <sdshlanta@gmail.com>
2018-01-22 19:10:06 -0600

    Added automatic updating to shit_time

--- doshit.sql
+++ doshit.sql
@@ -5,6 +5,6 @@ USE doshit;
 CREATE TABLE ashit (
 	shit_no int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	shit_type int(1) NOT NULL,
-	shit_time datetime NOT NULL,
+	shit_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	shit_length int(64) NOT NULL
 );
