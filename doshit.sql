11b5a7d7ec63b81aab99bd739d87151eaa85e13c
Mike Shlanta <sdshlanta@gmail.com>
2018-01-25 17:34:37 -0600

    afsjd;lk

--- doshit.sql
+++ doshit.sql
@@ -6,12 +6,14 @@ CREATE TABLE ashit (
 	shit_no int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	shit_type int(3) NOT NULL,
 	shit_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	shit_length float(64) NOT NULL
+	shit_length int(64) NOT NULL
 );
 
 CREATE TABLE users (
 	user_no int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_name varchar(50) NOT NULL,
 	user_passwd char(8) NOT NULL,
-	user_type int(3) NOT NULL
+	user_type int(1) NOT NULL
 );
+
+INSERT INTO users (user_name, user_passwd, user_type) VALUES ('admin', 'password', 1)
