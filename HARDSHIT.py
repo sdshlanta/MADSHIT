7c00486cf8d9685d7e186ca6ac7d87723ea7b687
sdshlanta <sdshlanta@github.com>
2018-01-25 18:10:51 +0000

    fixed syntax error

--- HARDSHIT.py
+++ HARDSHIT.py
@@ -58,7 +58,7 @@ def main():
 			for shit_no, shit_length, shit_type in cur:
 				if shit_no > latestSHITNo:
 					latestSHITNo = shit_no
-					if shit_type == 5
+					if shit_type == 5:
 						if aCurrentSHIT is not None:
 							aCurrentSHIT.cancel()
 							stopASHIT()
