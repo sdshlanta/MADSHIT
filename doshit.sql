2464ded3d3b1c49fc7b4ebdf2a0c0333cb2c3087
Mike Shlanta <sdshlanta@gmail.com>
2018-01-25 16:17:16 -0600

    Added support for web app users.

--- doshit.sql
+++ doshit.sql
@@ -8,3 +8,10 @@ CREATE TABLE ashit (
 	shit_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	shit_length float(64) NOT NULL
 );
+
+CREATE TABLE users (
+	user_no int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_name varchar(50) NOT NULL,
+	user_passwd char(8) NOT NULL,
+	user_type int(3) NOT NULL
+);
