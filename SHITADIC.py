b1dce2cadf5c2e649139d371b7d2e9ecd6d7d519
Mike Shlanta <sdshlanta@gmail.com>
2018-01-25 18:11:02 -0600

    Forgot to parse args

--- SHITADIC.py
+++ SHITADIC.py
@@ -18,6 +18,7 @@ def index():
 		else:
 			username = None
 		return render_template('index.html', name=username, error=error)
+	
 
 def main():
 
@@ -30,5 +31,6 @@ if __name__ == '__main__':
 	parser.add_argument("-H", "--databaseHost", type=str, help="The host the database is running on.  Default is 127.0.0.1", default="127.0.0.1")
 	parser.add_argument("-U", "--databaseUsername", type=str, help="Username to be used for the database connection. Default is root", default="daSHIT")
 	parser.add_argument("-P", "--databasePassword", type=str, help="Password to be used for the database connection. Default is blank", default="")
+	args = parser.parse_args()
 	db = SHITDB.SHITdb(args.host, args.database, args.username, args.username)
 	main()
