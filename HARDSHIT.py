4422d9977081089aca77cbde0e10f17b32061cf8
Mike Shlanta <sdshlanta@gmail.com>
2018-02-11 20:09:27 -0600

    debugging

--- HARDSHIT.py
+++ HARDSHIT.py
@@ -34,6 +34,7 @@ def main():
 	db = SHITDB.SHITdb(args.databaseHost, args.databaseName, args.databaseUsername, 
 					   args.databasePassword)
 	latestSHITNo = args.lastAlarmNumber
+	print(db.selectASpecficSHIT(1))
 	shit_no, shit_length, shit_type, shit_finished = db.selectASpecficSHIT(1)[0]
 	latestSHITLength = shit_length
 	aCurrentSHIT = threading.Timer(0, stopASHIT, args=(shit_no, db))
