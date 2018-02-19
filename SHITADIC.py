764ec9bc161287f1f93e4475c7a75913bf8b1fb3
Mike Shlanta <sdshlanta@gmail.com>
2018-02-19 14:46:48 -0600

    Still  trying to fix WEP

--- SHITADIC.py
+++ SHITADIC.py
@@ -216,15 +216,15 @@ def configWireless():
 	try:
 		if form['wirelessEncryption'] == 'WEP':
 			command = "sudo bash -c \"echo -n '%s' > /etc/wpa_supplicant/wpa_supplicant.conf\""
-			configString = 'ctrl_interface=/var/run/wpa_supplicant\n\
-			ctrl_interface_group=wheel\n\
-			network={\n\
-				ssid="%s"\n\
-				scan_ssid=1\n\
-				key_mgmt=NONE\n\
-				wep_tx_keyidx=0\n\
-				wep_key0=%s\
-			}'
+			configString = '''ctrl_interface=/var/run/wpa_supplicant\n
+			ctrl_interface_group=wheel
+			network={
+				ssid="%s"
+				scan_ssid=1
+				key_mgmt=NONE
+				wep_tx_keyidx=0
+				wep_key0=%s
+			}'''
 			commandToExecute = command % (configString % (form['wirelessSSID'], form['wirelessPassword']))
 			os.system(commandToExecute)
 			os.system('sudo pkill wpa_supplicant')
